a1eadabfb071949259f61e77ac9d06d1253af37f
Mussawer Rehan <mussawerrehan@gmail.com>
2020-10-05 17:26:17 +0500

    Add show and delete functionality

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,21 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticlesController extends AbstractController
 {
     /**
      * @Route("/articles", name="articles")
-     * @Method({"GET"})
+     * Method({"GET"})
      */
     public function index()
     {
-        $articles = ['Article 1', 'Article 2'];
-        return $this->render('articles/index.html.twig', [
-            'articles' => $articles,
-        ]);
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET","POST"})
+     */
+    public function new(Request $request) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title',TextType::class, array('attr' =>
+                array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save',SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array('article' => $article));
+    }
+
 }
