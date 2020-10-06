b60250b872142f4a4f3e82b6550a23a7ca869e8b
Mussawer Rehan <mussawerrehan@gmail.com>
2020-10-06 15:55:46 +0500

    Add edit articles functionality

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -62,12 +62,62 @@ class ArticlesController extends AbstractController
     }
 
     /**
+     * @Route("/article/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+        $article = new Article();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Update',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
      * @Route("/article/{id}", name="article_show")
      */
     public function show($id) {
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
         return $this->render('articles/show.html.twig', array('article' => $article));
+    }
+
+    /**
+     * @Route("/article/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
     }
 
 }
